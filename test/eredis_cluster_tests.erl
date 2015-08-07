fb51f6fe275126ab8beda471466bde253a7cdfdf
Cain <cain@0qian.com>
2015-08-07 16:49:24 +0800

    add transaction support

--- test/eredis_cluster_tests.erl
+++ test/eredis_cluster_tests.erl
@@ -45,3 +45,23 @@ basic_test_() ->
       ]
     }
 }.
+
+transaction_test_() ->
+    {inparallel,
+        {setup, ?Setup, ?Clearnup,
+        [
+            { "transaction",
+            fun () ->
+                ?assertMatch({ok, _},  eredis_cluster:transaction(fun (Worker) ->
+                    gen_server:call(Worker, {q, ["SET", "aa", "111"]}),
+                    gen_server:call(Worker, {q, ["DEL", "aa"]})
+                end, "aa")),
+                ?assertMatch({error, _},  eredis_cluster:transaction(fun (Worker) ->
+                    gen_server:call(Worker, {q, ["SET", "aa", "111"]}),
+                    gen_server:call(Worker, {q, ["DEL", "bb"]})
+                end, "aa"))
+            end
+            }
+        ]
+        }
+    }.
