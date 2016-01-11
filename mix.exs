95b36c5f23b008a909b7ae2fa386023859853fa8
Adrien Moreau <adrien@tongrd.com>
2016-01-11 06:10:17 +0000

    publish library on hex.pm

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule EredisCluster.Mixfile do
+  use Mix.Project
+
+  @version File.read!("VERSION") |> String.strip
+
+  def project do
+    [app: :eredis_cluster,
+     version: @version,
+     description: "An erlang wrapper for eredis library to support cluster mode",
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:poolboy, "~> 1.5.1"},
+      {:eredis, "~> 1.0.8"}]
+  end
+
+  defp package do
+    [files: ~w(src rebar.config README.md LICENSE VERSION),
+     maintainers: ["Adrien Moreau"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/adrienmo/eredis_cluster"}]
+  end
+end
