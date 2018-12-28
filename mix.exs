e1940692e675c5d68addc9ce01cc5bae67a8f173
getong <3949379+getong@users.noreply.github.com>
2018-12-28 09:53:31 +0000

    merge into one function

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EredisCluster.Mixfile do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.trim
+  @version String.trim(File.read!("VERSION"))
 
   def project do
     [app: :eredis_cluster,
