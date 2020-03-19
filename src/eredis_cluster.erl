5cba4aaa90ae70be0ceec587d4eabc76aecf649e
Jerry Luk <jerry@loopapps.com>
2020-03-18 21:08:08 -0700

    Reduce retry TTL and avoid eredis cluster monitor to call refresh_mapping when eredis returns {error, no_connection}

--- src/eredis_cluster.erl
+++ src/eredis_cluster.erl
@@ -194,7 +194,10 @@ handle_transaction_result(Result, Version) ->
         % If we detect a node went down, we should probably refresh the slot
         % mapping.
         {error, no_connection} ->
-            eredis_cluster_monitor:refresh_mapping(Version),
+            % Commented out refresh_mapping as most of the no_connection error are not due to the node went down.
+            % In the case where the eredis under high load and returns error due to time out, calling 
+            % refresh_mapping will further add more load the system.
+            % eredis_cluster_monitor:refresh_mapping(Version),
             retry;
 
         % If all poolboy workers are busy, we will try. We don't need to refresh the
